882bc961bea820a894c58a264ac9565a494a0d73
DmitryIgnatev <106831531+DmitryIgnatev@users.noreply.github.com>
2024-05-23 10:41:57 +0400

    feat: добавил шим-анимацию в карточку со скелетоном (#67)

--- lib/screens/notifications_screen/widgets/skeleton_listview/skeleton_card.dart
+++ lib/screens/notifications_screen/widgets/skeleton_listview/skeleton_card.dart
@@ -1,70 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 class SkeletonCard extends StatelessWidget {
-  const SkeletonCard({
-    super.key,
-  });
+  const SkeletonCard({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(
-        bottom: 12,
-      ),
+      padding: const EdgeInsets.only(bottom: 12),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(8),
         child: Container(
           color: Colors.white,
-          child: Padding(
-            padding: const EdgeInsets.all(12),
+          child: const Padding(
+            padding: EdgeInsets.all(12),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  height: 14,
-                  width: 126,
-                  decoration: BoxDecoration(
-                      color: const Color.fromRGBO(230, 230, 230, 1),
-                      borderRadius: BorderRadius.circular(4)),
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
+                SkeletonBox(height: 14, width: 126),
+                SizedBox(height: 8),
                 Row(
                   children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(4),
-                        color: const Color.fromRGBO(230, 230, 230, 1),
-                      ),
-                      height: 16,
-                      width: 16,
-                    ),
-                    const SizedBox(
-                      width: 8,
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(4),
-                        color: const Color.fromRGBO(230, 230, 230, 1),
-                      ),
-                      height: 16,
-                      width: 84,
-                    ),
+                    SkeletonBox(height: 16, width: 16),
+                    SizedBox(width: 8),
+                    SkeletonBox(height: 16, width: 84),
                   ],
                 ),
-                const SizedBox(
-                  height: 8,
-                ),
-                Container(
+                SizedBox(height: 8),
+                SkeletonBox(
                   height: 32,
-                  decoration: BoxDecoration(
-                      color: const Color.fromRGBO(204, 204, 204, 1),
-                      borderRadius: BorderRadius.circular(4)),
-                )
+                  width: double.infinity,
+                  color: Color.fromRGBO(204, 204, 204, 1),
+                ),
               ],
             ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class SkeletonBox extends StatelessWidget {
+  final double height;
+  final double width;
+  final Color color;
+  final double borderRadius;
+
+  const SkeletonBox({
+    super.key,
+    required this.height,
+    required this.width,
+    this.color = const Color.fromRGBO(230, 230, 230, 1),
+    this.borderRadius = 4,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Shimmer.fromColors(
+      baseColor: color,
+      highlightColor: Colors.grey[300]!,
+      child: Container(
+        height: height,
+        width: width,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(borderRadius),
         ),
       ),
     );
