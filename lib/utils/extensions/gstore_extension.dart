56097427ed8267d1d0deebe03c416b91b7b4912f
Marina Bukhanovskaya <116250199+bukhanovskayamarina22@users.noreply.github.com>
2024-06-05 18:19:42 +0300

    refactor: переписала updateLocally #305305 (#168)

--- lib/utils/extensions/gstore_extension.dart
+++ lib/utils/extensions/gstore_extension.dart
@@ -1,21 +1,14 @@
-import 'dart:collection';
-
 import 'package:unityspace/models/model_interfaces.dart';
 import 'package:wstore/wstore.dart';
 
-extension GStoreExtension on GStore {
-  List<Identifiable> updateLocally(
-    List<Identifiable> list,
-    Map<int, Identifiable> map,
+extension GStoreExtension<T extends Identifiable> on GStore {
+  List<T> updateLocally(
+    List<T> list,
+    Map<int, T> map,
   ) {
-    final List<Identifiable> baseList = List<Identifiable>.from(list);
-    final HashMap<int, Identifiable> baseMap =
-        HashMap<int, Identifiable>.from(map);
-    for (final Identifiable object in baseList) {
-      baseMap[object.id] = object;
+    for (final T object in list) {
+      map[object.id] = object;
     }
-    final List<Identifiable> newList =
-        baseMap.entries.map((element) => element.value).toList();
-    return newList;
+    return List<T>.from(map.entries.map((element) => element.value));
   }
 }
